401e14f4d025feea04bcfe8a4ddfce308ab329ef
jgum63 <joshgum3@gmail.com>
2023-04-06 21:53:57 -0500

    added last array and finished function

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,7 @@ var generateBtn = document.querySelector("#generate");
 let lowerAlpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 let upperAlpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let specialChar = ['!', '@', '#', '$', '%', '&', '?']
 
 let builtArray = [];
 
@@ -24,7 +25,13 @@ function generatePassword() {
 
   if (confirm("Do you want your password to have numbers?")) {
 
-    builtArray = [...builtArray, ...numbers]
+    builtArray = [...builtArray, ...numbers];
+    
+  }
+
+  if (confirm("Do you want your password to have special characters?")) {
+
+    builtArray = [...builtArray, ...specialChar];
     
   }
 
